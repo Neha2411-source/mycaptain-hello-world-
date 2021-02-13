5b66ee65775173d0d8404758068e2a9fd06056ff
Neha2411-source <78656805+Neha2411-source@users.noreply.github.com>
2021-02-13 10:37:23 +0530

    Update Samplehello.java

--- src/samplehello/Samplehello.java
+++ src/samplehello/Samplehello.java
@@ -17,7 +17,7 @@ public class Samplehello {
     public static void main(String[] args) {
        
         // TODO code application logic here
-       System.out.println("neha");
+       System.out.println("hello world");
     }
     
     
